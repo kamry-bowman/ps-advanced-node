da096382d42a49310b2417a900ae42ef5e96a249
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-16 13:53:48 -0700

    Implement basic dns module commands

---
+++ networking/dns.js
@@ -0,0 +1,17 @@
+const dns = require("dns");
+
+// dns.lookup('pluralsight.com', (err, address) => {
+//   console.log(address);
+// });
+
+// dns.resolve("pluralsight.com", "A", (err, address) => {
+//   console.log(address);
+// });
+
+// dns.resolveMx("pluralsight.com", (err, address) => {
+//   console.log(address);
+// });
+
+dns.reverse('35.161.75.227', (err, hostnames) => {
+  console.log(hostnames);
+});
