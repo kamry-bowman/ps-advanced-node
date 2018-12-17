3bea63f3c3dcdcf9abbcc287491960333f2f96c6
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-16 19:17:24 -0700

    implement https

--- http/http.js
+++ http/http.js
@@ -1,18 +1,21 @@
-const server = require("http").createServer();
+const fs = require("fs");
+const server = require("https").createServer({
+  key: fs.readFileSync("./key.pem"),
+  cert: fs.readFileSync("./cert.pem")
+});
 
 server.on("request", (req, res) => {
   res.writeHead(200, { "content-type": "text/plain" });
   res.write("Hello world\n");
 
   setTimeout(() => {
-    res.write('Another Hello World\n');
+    res.write("Another Hello World\n");
   }, 1000);
 
   setTimeout(() => {
-    res.write('Another Hello World\n');
+    res.write("Another Hello World\n");
     res.end();
   }, 2000);
-  
 });
 
-server.listen(8000);
+server.listen(443);
