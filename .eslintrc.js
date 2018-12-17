1fb581350e81897f5241bf78e931584bb4a6eebe
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-16 20:09:17 -0700

    implement routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
 module.exports = {
-    "env": {
-        "node":true,
-        "browser": false,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2016
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "no-console": 1,
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    node: true,
+    browser: false,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2016
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    'no-console': 1,
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
 };
